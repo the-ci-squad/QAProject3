27316407cc827b0152080eff7523324a3d464272
tinokingstone <tinokingstone@gmail.com>
2020-04-23 13:32:54 +0100

    ansible roles and terraform 100% working

--- tests/development-Database.tf
+++ tests/development-Database.tf
@@ -1,17 +1,19 @@
 
-module "CiServer" {
+module "Database" {
   source                        = "Azure/compute/azurerm"
   resource_group_name           = azurerm_resource_group.dev_environment.name
-  vm_hostname                   = "CiServer"
-  public_ip_dns                 = ["cidfsfdsfdsf"]
+  vm_hostname                   = "Database"
+
+  public_ip_dns                 = ["devdbdfsfdsfdsf"]
   nb_public_ip                  = "1"
+
   remote_port                   = "22"
   nb_instances                  = 1
   vm_os_publisher               = "Canonical"
   vm_os_offer                   = "UbuntuServer"
   vm_os_sku                     = "18.04-LTS"
   admin_password                = "password"
-  admin_username                = "jenkins"
+  admin_username                = "app-dev"
   vnet_subnet_id                = module.network.vnet_subnets[0]
   boot_diagnostics              = true
   delete_os_disk_on_termination = true
@@ -31,12 +33,11 @@ module "CiServer" {
 }
 
 
-output "CiServer_vm_private_ips" {
-  value = module.CiServer.network_interface_private_ip
+output "Database_vm_private_ips" {
+  value = module.Database.network_interface_private_ip
 }
 
-
-output "CiServer_vm_public_ip" {
-  value = module.CiServer.public_ip_address
+output "Database_vm_public_ip" {
+  value = module.Database.public_ip_address
 }
 
---
+++ tests/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "dev_environment" {
+  name     = "dev_environment"
+  location = "uk south"
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  version             = "3.0.1"
+  resource_group_name = azurerm_resource_group.dev_environment.name
+  subnet_prefixes     = ["10.0.1.0/24"]
+}
